a7f6f9321e4323456bf4eed4ad57d28207053835
DmyDylan <59020544+DmyDylan@users.noreply.github.com>
2021-07-01 14:45:25 +0200

    Add files via upload

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import * as meta from './package.json';
+import resolve from 'rollup-plugin-node-resolve';
+
+const onwarn = function(warning, warn) {
+  if (warning.code === 'CIRCULAR_DEPENDENCY') {
+    return;
+  }
+  warn(warning);
+};
+const output = {
+  file: `public/main.js`,
+  name: '${meta.name}',
+  format: 'iife',
+  indent: false,
+  extend: true,
+  banner: `// ${meta.homepage} v${
+    meta.version
+  } Copyright ${new Date().getFullYear()} ${meta.author.name}`,
+};
+
+export default {
+  input: 'src/main.js',
+  onwarn: onwarn,
+  output: output,
+  plugins: [resolve()],
+};
